a2dc1ab60f625f2da7eda5f6609a1bc885607d90
Sandun Tharaka <67332822+sanduntharaka@users.noreply.github.com>
2025-03-05 15:13:26 +0530

    AM-228 fixed date and employee id

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { json } from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from "multer";
 import { join, dirname } from "path";
@@ -10,17 +11,17 @@ import { fileURLToPath } from "url";
 import documentRoutes from "./routes/documentRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.options("*", cors());
 
 // Middleware to parse JSON requests
 app.use(json());
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(cookieParser());
 
@@ -33,20 +34,20 @@ app.use(upload.single("file"));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Set the directory where your PDFs are stored
-const uploadsDir = join(__dirname, "uploads");
-app.use("/uploads", express.static(uploadsDir));
+// // Set the directory where your PDFs are stored
+// const uploadsDir = join(__dirname, "uploads");
+// app.use("/uploads", express.static(uploadsDir));
 
 // Routes
 app.use("/", documentRoutes);
 
-// Health check route
-app.get("/health", (req, res) => res.status(200).send("OK!"));
+// // Health check route
+// app.get("/health", (req, res) => res.status(200).send("OK!"));
 
 // Error handling middleware
 app.use(errorHandler);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
